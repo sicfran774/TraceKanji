55ad5b7bcf5cdd73a3ed6d8d02498c09d2e8919c
sicfran774 <sicfran.774@gmail.com>
2024-03-03 10:02:28 -0800

    Force dynamic for cron job to build as server comp

--- src/app/api/emailer/route.js
+++ src/app/api/emailer/route.js
@@ -1,6 +1,7 @@
 import { separateAccounts } from "@/lib/emailer/emailer"
 import { NextResponse } from "next/server"
 
+export const dynamic = 'force-dynamic'
 export async function GET(request) {
 
     const response = separateAccounts()
