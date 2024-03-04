f98236d2676132ca93dfdaeff4bb191e81160737
sicfran774 <sicfran.774@gmail.com>
2024-03-04 00:55:25 -0800

    Fix fetch

--- src/lib/emailer/emailer.js
+++ src/lib/emailer/emailer.js
@@ -1,7 +1,17 @@
 export async function sendEmails(){
-    const result = await fetch(process.env.FLASK_ENDPOINT + "api/emailer", {
-        method: "GET"
-    })
-    console.log(result)
-    return result
+    fetch(`${process.env.FLASK_ENDPOINT}api/emailer`)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Parse the JSON response
+        })
+        .then(data => {
+            // Process the JSON data
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error)
+            return error
+        })
 }
